6ba24b36c412c24910fbe7fe46881c53a25b7c06
emef2247 <emef2247@gmail.com>
2026-09-26 08:51:19 +0900

    bring py/opll.py from vgm2tx802 project

--- py/opll.py
+++ py/opll.py
@@ -143,12 +143,38 @@ RHYTHM_CH_MAP = {
     "hh": 13,  # Hi-Hat
 }
 
+RHYTHM_CH_MAP_TX802 = {
+    "bd": 9,
+    "sd": 9,
+    "tom": 9,
+    "tc": 9,
+    "hh": 9,
+}
+
 RHYTHM_VOICE_ID_MAP = {
     "bd": 16,
     "sd": 17,
     "tom": 18,
     "tc": 19,
     "hh": 20,
+}
+
+# RX21 Instrument Number
+# BD        45(A1)
+# TOM3      48(C2)
+# TOM2      50(D2)
+# SD        52 (E2)
+# TOM1      53(F2)
+# CLAPS     54 (F#2)
+# HH CLOSED 57 (A2)
+# HH OPEN   59 (B2)
+# CYM       60 (C3)
+RHYTHM_VOICE_ID_MAP_RX21 = {
+    "bd":  45,
+    "sd":  52,
+    "tom": 53,
+    "tc":  60,
+    "hh":  57,
 }
 
 NUM_CH = 18  # 0-8: OPLL, 13-17: Rhythm など
